2fc2245c1e959b1447468138249ef357c3146bcc
ccconquer <111716969+ccconquer@users.noreply.github.com>
2024-03-24 17:31:35 +0800

    Add files via upload

---
+++ agents/traffic_agent.py
@@ -0,0 +1,63 @@
+from typing import List
+
+import igp2 as ip
+from igp2.agents.macro_agent import MacroAgent
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class TrafficAgent(MacroAgent):
+    """ Agent that follows a list of MAs calculated using A*. """
+
+    def __init__(self, agent_id: int, initial_state: ip.AgentState, goal: "ip.Goal" = None, fps: int = 20):
+        super(TrafficAgent, self).__init__(agent_id, initial_state, goal, fps)
+        self._astar = ip.AStar(max_iter=1000)
+        self._macro_list = []
+
+    def set_macro_actions(self, new_macros: List[ip.MacroAction]):
+        """ Specify a new set of macro actions to follow. """
+        assert len(new_macros) > 0, "Empty macro list given!"
+        self._macro_list = new_macros
+
+    def set_destination(self, observation: ip.Observation, goal: ip.Goal = None):
+        """ Set the current destination of this vehicle and calculate the shortest path to it using A*.
+
+            Args:
+                observation: The current observation.
+                goal: Optional new goal to override the current one.
+        """
+        if goal is not None:
+            self._goal = goal
+
+        logger.debug(f"Finding path for TrafficAgent ID {self.agent_id}")
+        _, actions = self._astar.search(self.agent_id,
+                                        observation.frame,
+                                        self._goal,
+                                        observation.scenario_map,
+                                        open_loop=False)
+        self._macro_list = actions[0]
+
+    def done(self, observation: ip.Observation) -> bool:
+        """ Returns true if there are no more actions on the macro list and the current macro is finished. """
+        return len(self._macro_list) == 0 and super(TrafficAgent, self).done(observation)
+
+    def next_action(self, observation: ip.Observation) -> ip.Action:
+        if self.current_macro is None:
+            if len(self._macro_list) == 0:
+                self.set_destination(observation)
+            self._advance_macro()
+
+        if self._current_macro.done(observation):
+            if len(self._macro_list) > 0:
+                self._advance_macro()
+            else:
+                return ip.Action(0, 0)
+
+        return self._current_macro.next_action(observation)
+
+    def next_state(self, observation: ip.Observation) -> ip.AgentState:
+        return super(TrafficAgent, self).next_state(observation)
+
+    def _advance_macro(self):
+        self._current_macro = self._macro_list.pop(0)
---
+++ carla/carla_agent_wrapper.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import igp2 as ip
+import carla
+from typing import Optional
+
+
+class CarlaAgentWrapper:
+    """ Wrapper class that provides a simple way to retrieve control for the attached actor. """
+    def __init__(self, agent: ip.Agent, actor: carla.Actor):
+        self.__agent = agent
+        self.__actor = actor
+        self.__name = self.__actor.attributes["role_name"]
+
+    def __repr__(self):
+        return f"Actor {self.actor_id}; Agent {self.agent_id}"
+
+    def next_control(self, observation: ip.Observation) -> Optional[carla.VehicleControl]:
+        limited_observation = self._apply_view_radius(observation)
+        action = self.__agent.next_action(limited_observation)
+        if action is None or self.__agent.done(observation):
+            return None
+
+        control = carla.VehicleControl()
+        norm_acceleration = action.acceleration / observation.frame[self.agent_id].metadata.max_acceleration
+        if action.acceleration >= 0:
+            control.throttle = min(1., norm_acceleration)
+            control.brake = 0.
+        else:
+            control.throttle = 0.
+            control.brake = min(-norm_acceleration, 1.)
+        control.steer = np.clip(-action.steer_angle, -1.0, 1.0)
+        control.hand_brake = False
+        control.manual_gear_shift = False
+
+        return control
+
+    def done(self, observation: ip.Observation) -> bool:
+        """ Returns whether the wrapped agent is done. """
+        return self.__agent.done(observation)
+
+    def _apply_view_radius(self, observation: ip.Observation):
+        if hasattr(self.agent, "view_radius"):
+            pos = observation.frame[self.agent_id].position
+            new_frame = {aid: state for aid, state in observation.frame.items()
+                         if np.linalg.norm(pos - state.position) <= self.agent.view_radius}
+            return ip.Observation(new_frame, observation.scenario_map)
+        return observation
+
+    @property
+    def state(self) -> ip.AgentState:
+        return self.agent.state
+
+    @property
+    def agent_id(self) -> int:
+        return self.__agent.agent_id
+
+    @property
+    def actor_id(self) -> int:
+        return self.__actor.id
+
+    @property
+    def actor(self) -> carla.Actor:
+        return self.__actor
+
+    @property
+    def agent(self) -> ip.Agent:
+        return self.__agent
+
+    @property
+    def name(self):
+        """ The role name of the wrapped Actor. """
+        return self.__name
